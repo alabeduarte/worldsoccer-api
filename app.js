4924f8842d7c86e87673e90ca9848ee6f2ca0619
Alabê Duarte <alabeduarte@gmail.com>
2013-12-08 12:40:04 -0300

    Changed dependencies to use player parser

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var app = require('./lib/server')(
   require('express')
 );
 
-var sourceHost = 'http://c3420952.r52.cf0.rackcdn.com';
-require('./lib/controllers/players.controller')(app, sourceHost, require('request'), require('xml2json'));
+var playerParser = require('./lib/player.parser')(require('xml2json'));
+require('./lib/controllers/players.controller')(app, 'http://c3420952.r52.cf0.rackcdn.com', require('request'), playerParser);
