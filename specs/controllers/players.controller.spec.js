0b50c9b341b91bb5624d91fe53a9462b1174f4f5
Alabê Duarte <alabeduarte@gmail.com>
2013-12-16 10:32:59 -0300

    Cleaning up

--- specs/controllers/players.controller.spec.js
+++ specs/controllers/players.controller.spec.js
@@ -8,7 +8,7 @@ var request = require(__dirname + '/requestTest')
   describe('GET /countries/bra/players', function() {
     before(function() {
       var xml = '<?xml version="1.0"?><PackData><PlayerData baseImageUrl="http://cdn.soccerwiki.org/images/player/"><P id="1" f="John" s="Doe" i="1.jpg"/><P id="2" f="Mary" s="Doe" i="2.jpg"/></PlayerData></PackData>';
-      nock(url) .get('/BRAplayerbasicdata.xml').reply(200, xml);
+      nock(url).get('/BRAplayerbasicdata.xml').reply(200, xml);
     });
     it('respond with json', function(done) {
       var expectedResponse = '[{"id":"1","fullName":"John Doe","firstName":"John","lastName":"Doe","photo":"http://cdn.soccerwiki.org/images/player/1.jpg"},{"id":"2","fullName":"Mary Doe","firstName":"Mary","lastName":"Doe","photo":"http://cdn.soccerwiki.org/images/player/2.jpg"}]';
@@ -19,7 +19,7 @@ var request = require(__dirname + '/requestTest')
   describe('GET /countries/ita/players', function() {
     before(function() {
       var xml = '<?xml version="1.0"?><PackData><PlayerData baseImageUrl="http://cdn.soccerwiki.org/images/player/"><P id="10" f="Roberto" s="Baggio" i="10.jpg"/><P id="21" f="Cristian" s="Vieri" i="21.jpg"/></PlayerData></PackData>';
-      nock(url) .get('/ITAplayerbasicdata.xml').reply(200, xml);
+      nock(url).get('/ITAplayerbasicdata.xml').reply(200, xml);
     });
     it('respond with json', function(done) {
       var expectedResponse = '[{"id":"10","fullName":"Roberto Baggio","firstName":"Roberto","lastName":"Baggio","photo":"http://cdn.soccerwiki.org/images/player/10.jpg"},{"id":"21","fullName":"Cristian Vieri","firstName":"Cristian","lastName":"Vieri","photo":"http://cdn.soccerwiki.org/images/player/21.jpg"}]';
